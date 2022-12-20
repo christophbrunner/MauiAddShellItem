3349b5d85cead8d72137396f1b6d02d83e46a328
Christoph Brunner <Info@brunner-sw.ch>
2022-12-20 22:49:20 +0100

    Add project files.

---
+++ MauiAddShellItem/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiAddShellItem;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiAddShellItem/AppShell.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace MauiAddShellItem;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await AddFlyoutItems();
+    }
+
+    public async Task AddFlyoutItems()
+    {
+        // Get  flyout items from Web
+        await Task.Delay(500);
+
+        for (int i = 0; i < 3; i++)
+        {
+            var flyoutItem = new FlyoutItem
+            {
+                Title = $"{i}-item",
+                Route = $"{i}-item"
+            };
+
+            flyoutItem.Items.Add(new ShellContent
+            {
+                Content = new MainPage(),
+                Route = $"{i}-item"
+            });
+
+            Items.Add(flyoutItem);
+        }
+    }
+}
